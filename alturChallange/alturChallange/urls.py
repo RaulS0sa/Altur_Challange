0c7a479a2b23f38e84f13a4fd87dadba2b03b2f3
RaulS0sa <raul.sosa.cortes@gmail.com>
2026-05-14 15:43:31 -0600

    Fix requirements, static files, and nested paths 15:43:31

--- alturChallange/alturChallange/urls.py
+++ alturChallange/alturChallange/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from alturChallange import views
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
